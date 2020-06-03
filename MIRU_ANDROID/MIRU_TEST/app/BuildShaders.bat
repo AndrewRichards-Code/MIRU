612d25204d5e7359328a51944e7d04077201f363
Andrew Richards <andyjrichards93@gmail.com>
2020-06-03 10:41:18 +0100

    Updated BuildShaders.bat files to use relative paths; added .keep files for maintain empty directories.

--- MIRU_ANDROID/MIRU_TEST/app/BuildShaders.bat
+++ MIRU_ANDROID/MIRU_TEST/app/BuildShaders.bat
@@ -1,12 +1,12 @@
-cd C:\Users\Andrew\source\repos\MIRU\MIRU_SHADER_COMPILER\exe\x64\Debug\
+cd %~dp0..\..\..\MIRU_SHADER_COMPILER\exe\x64\Debug\
 MIRU_SHADER_COMPILER.exe ^
--f:C:\Users\Andrew\source\repos\MIRU\MIRU_TEST/res/shaders/basic.vert.hlsl ^
--o:C:\Users\Andrew\source\repos\MIRU\MIRU_ANDROID/MIRU_TEST/app/src/main/assets/bin ^
--i:C:\Users\Andrew\source\repos\MIRU\MIRU_SHADER_COMPILER/shaders/includes ^
+-f:%~dp0..\..\..\MIRU_TEST/res/shaders/basic.vert.hlsl ^
+-o:%~dp0..\..\..\MIRU_ANDROID/MIRU_TEST/app/src/main/assets/bin ^
+-i:%~dp0..\..\..\MIRU_SHADER_COMPILER/shaders/includes ^
 -spv
 MIRU_SHADER_COMPILER.exe ^
--f:C:\Users\Andrew\source\repos\MIRU\MIRU_TEST/res/shaders/basic.frag.hlsl ^
--o:C:\Users\Andrew\source\repos\MIRU\MIRU_ANDROID/MIRU_TEST/app/src/main/assets/bin ^
--i:C:\Users\Andrew\source\repos\MIRU\MIRU_SHADER_COMPILER/shaders/includes ^
+-f:%~dp0..\..\..\MIRU_TEST/res/shaders/basic.frag.hlsl ^
+-o:%~dp0..\..\..\MIRU_ANDROID/MIRU_TEST/app/src/main/assets/bin ^
+-i:%~dp0..\..\..\MIRU_SHADER_COMPILER/shaders/includes ^
 -spv
 pause
