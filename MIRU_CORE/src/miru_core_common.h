3fc96dee6834fc2f7bb916a48869f3ca7dbe6575
Andrew Richards <andyjrichards93@gmail.com>
2020-07-14 18:22:30 +0100

    D3D12SetName() to_wstring conversion updated.

--- MIRU_CORE/src/miru_core_common.h
+++ MIRU_CORE/src/miru_core_common.h
@@ -264,10 +264,16 @@ namespace miru
 		if (!GraphicsAPI::IsSetNameAllowed())
 			return;
 
-		std::string _name = name;
-		std::wstring w_name(_name.begin(), _name.end());
+		auto to_wstring = [](const std::string& string) -> std::wstring
+		{
+			wchar_t* wstr = new wchar_t[string.size() + 1];
+			mbstowcs_s(nullptr, wstr, string.size() + 1, string.c_str(), string.size() + 1);
+			std::wstring result(wstr);
+			delete[] wstr;
+			return result;
+		};
 
-		reinterpret_cast<ID3D12Object*>(object)->SetName(w_name.c_str());
+		reinterpret_cast<ID3D12Object*>(object)->SetName(to_wstring(name).c_str());
 	}
 	#endif
 
