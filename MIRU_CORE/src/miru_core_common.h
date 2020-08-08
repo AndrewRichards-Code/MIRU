b8a59ed53c80dff596727d824c45fedf8031cc2e
Andrew Richards <andyjrichards93@gmail.com>
2020-08-08 09:29:06 +0100

    Minor update.

--- MIRU_CORE/src/miru_core_common.h
+++ MIRU_CORE/src/miru_core_common.h
@@ -113,8 +113,8 @@ namespace miru
 	template<class T>
 	using Scope = std::unique_ptr<T>;
 
-	template<typename T, typename ... Args>
-	constexpr Scope<T> CreateScope(Args&& ... args)
+	template<typename T, typename... Args>
+	constexpr Scope<T> CreateScope(Args&&... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
@@ -122,8 +122,8 @@ namespace miru
 	template<class T>
 	using Ref = std::shared_ptr<T>;
 
-	template<typename T, typename ... Args>
-	constexpr Ref<T> CreateRef(Args&& ... args)
+	template<typename T, typename... Args>
+	constexpr Ref<T> CreateRef(Args&&... args)
 	{
 		return std::make_shared<T>(std::forward<Args>(args)...);
 	}
