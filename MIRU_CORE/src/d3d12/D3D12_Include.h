3bb52cba608ac1c635ddb5887177f3a16dae62e5
Andrew Richards <andyjrichards93@gmail.com>
2023-06-11 15:04:32 +0100

    Updated D3D12_Include.h for D3D12 Agility SDK 1.610.3.

--- MIRU_CORE/src/d3d12/D3D12_Include.h
+++ MIRU_CORE/src/d3d12/D3D12_Include.h
@@ -1,14 +1,14 @@
 #pragma once
 #if defined(MIRU_D3D12)
 //D3D12 Header
-#include "../packages/Microsoft.Direct3D.D3D12.1.608.3/build/native/include/d3d12.h"
+#include "../packages/Microsoft.Direct3D.D3D12.1.610.3/build/native/include/d3d12.h"
 #include <dxgi1_6.h>
-#include "../packages/Microsoft.Direct3D.D3D12.1.608.3/build/native/include/d3dx12/d3dx12.h"
+#include "../packages/Microsoft.Direct3D.D3D12.1.610.3/build/native/include/d3dx12/d3dx12.h"
 
 #define MIRU_D3D12_AGILITY_SDK_SET_VERSION_AND_PATH \
 extern "C"\
 {\
-	__declspec(dllexport) extern const unsigned int D3D12SDKVersion = 608;\
+	__declspec(dllexport) extern const unsigned int D3D12SDKVersion = 610;\
 	__declspec(dllexport) extern const char* D3D12SDKPath = ".\\D3D12\\";\
 }
 
